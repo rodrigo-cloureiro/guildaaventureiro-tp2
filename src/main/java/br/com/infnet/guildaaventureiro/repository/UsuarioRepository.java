8aab7084c4204a4289324a37594a054418bea25a
rodrigo.loureiro <rodrigo.loureiro@sondotecnica.com.br>
2026-03-08 01:28:36 -0300

    criando metodo de consulta findByEmailIgnoreCase; renomeando o parametro do método findByOrganizacaoNome

--- src/main/java/br/com/infnet/guildaaventureiro/repository/UsuarioRepository.java
+++ src/main/java/br/com/infnet/guildaaventureiro/repository/UsuarioRepository.java
@@ -3,17 +3,21 @@ package br.com.infnet.guildaaventureiro.repository;
 import br.com.infnet.guildaaventureiro.domain.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     @Query(value = """
-            SELECT u\s
-            FROM Usuario u\s
-            JOIN u.organizacao o
-            WHERE UPPER(o.nome) = UPPER(:organizacaoName)
-           \s""")
-    List<Usuario> findByOrganizacaoNomeIgnoreCase(String organizacaoName);
+             SELECT u\s
+             FROM Usuario u\s
+             JOIN u.organizacao o
+             WHERE UPPER(o.nome) = UPPER(:organizacaoNome)
+            \s""")
+    List<Usuario> findByOrganizacaoNomeIgnoreCase(@Param("organizacaoNome") String organizacaoNome);
+
+    Optional<Usuario> findByEmailIgnoreCase(String email);
 }
