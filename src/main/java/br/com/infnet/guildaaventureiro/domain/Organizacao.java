664e3b8755d614eaaccb533af27dd534981c95fb
rodrigo.loureiro <rodrigo.loureiro@sondotecnica.com.br>
2026-03-07 23:16:46 -0300

    adicionando unique constraint de nome na entidade; definindo schema da sequence; adicoes e correcoes no mapeamento da entidade

--- src/main/java/br/com/infnet/guildaaventureiro/domain/Organizacao.java
+++ src/main/java/br/com/infnet/guildaaventureiro/domain/Organizacao.java
@@ -6,10 +6,17 @@ import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
-@Table(name = "organizacoes", schema = "audit")
+@Table(
+        name = "organizacoes",
+        schema = "audit",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "organizacoes_nome_key", columnNames = {"nome"})
+        }
+)
 @Getter
 @Setter
 public class Organizacao {
@@ -18,21 +25,19 @@ public class Organizacao {
     @SequenceGenerator(
             name = "organizacoes_id",
             sequenceName = "organizacoes_id_seq",
+            schema = "audit",
             allocationSize = 1
     )
     private Long id;
 
     @Column(
-            name = "nome",
             length = 120,
-            nullable = false
+            nullable = false,
+            unique = true
     )
     private String nome;
 
-    @Column(
-            name = "ativo",
-            nullable = false
-    )
+    @Column(nullable = false)
     private boolean ativo = true;
 
     @CreationTimestamp
@@ -44,5 +49,16 @@ public class Organizacao {
     private LocalDateTime createdAt;
 
     @OneToMany(mappedBy = "organizacao", fetch = FetchType.LAZY)
-    private List<Usuario> usuarios;
+    private Set<Usuario> usuarios = new HashSet<>();
+
+    @OneToMany(mappedBy = "organizacao", fetch = FetchType.LAZY)
+    private Set<Role> roles = new HashSet<>();
+
+    @OneToMany(mappedBy = "organizacao", fetch = FetchType.LAZY)
+    private Set<ApiKey> apiKeys = new HashSet<>();
+
+    public void adicionarUsuario(Usuario usuario) {
+        this.usuarios.add(usuario);
+        usuario.definirOrganizacao(this);
+    }
 }
