ac8815ac1f5b8e7a6e91769079947cd19c56f4cd
rodrigo.loureiro <rodrigo.loureiro@sondotecnica.com.br>
2026-03-08 00:16:49 -0300

    criando enum para action do AuditEntry

---
+++ src/main/java/br/com/infnet/guildaaventureiro/domain/enums/AuditEntryAction.java
@@ -0,0 +1,12 @@
+package br.com.infnet.guildaaventureiro.domain.enums;
+
+public enum AuditEntryAction {
+    CREATE,
+    UPDATE,
+    DELETE,
+    LOGIN,
+    LOGOUT,
+    EXPORT,
+    IMPORT,
+    ERROR
+}
