c4698cf45ce4243f9afee7b8205b7512e5273e70
rodrigo.loureiro <rodrigo.loureiro@sondotecnica.com.br>
2026-03-12 00:27:09 -0300

    implementando busca por filtro no repository com query JPQL

--- src/main/java/br/com/infnet/guildaaventureiro/repository/aventura/AventureiroRepository.java
+++ src/main/java/br/com/infnet/guildaaventureiro/repository/aventura/AventureiroRepository.java
@@ -1,9 +1,30 @@
 package br.com.infnet.guildaaventureiro.repository.aventura;
 
 import br.com.infnet.guildaaventureiro.domain.aventura.Aventureiro;
+import br.com.infnet.guildaaventureiro.domain.aventura.enums.AventureiroClasse;
+import br.com.infnet.guildaaventureiro.dto.AventureiroResponseDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AventureiroRepository extends JpaRepository<Aventureiro, Long> {
+    @Query(value = """
+            SELECT new br.com.infnet.guildaaventureiro.dto.AventureiroResponseDto(
+                        a.id, a.nome, a.classe, a.nivel, a.ativo
+            )
+            FROM Aventureiro a
+            WHERE (:status IS NULL OR a.ativo = :status) AND
+            (:classe IS NULL OR a.classe = :classe) AND
+            (:nivelMinimo IS NULL OR a.nivel >= :nivelMinimo)
+            """)
+    Page<AventureiroResponseDto> findByStatusAndClasseAndNivelMinimo(
+            @Param(value = "status") Boolean status,
+            @Param(value = "classe") AventureiroClasse classe,
+            @Param(value = "nivelMinimo") Integer nivelMinimoInteger,
+            Pageable pageable
+    );
 }
