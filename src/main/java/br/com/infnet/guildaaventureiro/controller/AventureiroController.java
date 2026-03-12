748e9e1c9754c21b7954fdf5e83ea1c5f97d848d
rodrigo.loureiro <rodrigo.loureiro@sondotecnica.com.br>
2026-03-12 00:30:00 -0300

    implementando controller com metodo POST para criacao de aventureiros e GET para listagem paginada de aventureiros

---
+++ src/main/java/br/com/infnet/guildaaventureiro/controller/AventureiroController.java
@@ -0,0 +1,62 @@
+package br.com.infnet.guildaaventureiro.controller;
+
+import br.com.infnet.guildaaventureiro.domain.aventura.Aventureiro;
+import br.com.infnet.guildaaventureiro.dto.AventureiroCreateDto;
+import br.com.infnet.guildaaventureiro.dto.AventureiroFiltroRequestDto;
+import br.com.infnet.guildaaventureiro.dto.AventureiroResponseDto;
+import br.com.infnet.guildaaventureiro.mapper.AventureiroMapper;
+import br.com.infnet.guildaaventureiro.service.AventureiroService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/aventureiro")
+public class AventureiroController {
+    private final AventureiroService aventureiroService;
+
+    // ===================
+    // Listar Aventureiros
+    // ===================
+    @GetMapping(value = "")
+    public ResponseEntity<List<AventureiroResponseDto>> listarAventureiros(
+            @Valid AventureiroFiltroRequestDto filtro,
+            @RequestHeader(value = "X-Page", required = false, defaultValue = "0")
+            int page,
+            @RequestHeader(value = "X-Size", required = false, defaultValue = "10")
+            int size,
+            @RequestHeader(value = "X-Sort-By", required = false, defaultValue = "id")
+            String sortBy,
+            @RequestHeader(value = "X-Sort-Direction", required = false, defaultValue = "ASC")
+            String sortDirection
+    ) {
+        Sort sort = Sort.by(
+                sortDirection.equalsIgnoreCase("DESC") ? Sort.Direction.DESC : Sort.Direction.ASC,
+                sortBy
+        );
+        Page<AventureiroResponseDto> aventureiroPage = aventureiroService.listar(filtro, page, size, sort);
+        return ResponseEntity.ok()
+                .header("X-Page", String.valueOf(aventureiroPage.getNumber()))
+                .header("X-Size", String.valueOf(aventureiroPage.getSize()))
+                .header("X-Total-Count", String.valueOf(aventureiroPage.getNumberOfElements()))
+                .header("X-Total-Pages", String.valueOf(aventureiroPage.getTotalPages()))
+                .body(aventureiroPage.getContent());
+    }
+
+    // =====================
+    // Registrar Aventureiro
+    // =====================
+    @PostMapping(value = "")
+    public ResponseEntity<AventureiroResponseDto> registrarAventureiro(@RequestBody @Valid AventureiroCreateDto dto) {
+        Aventureiro aventureiro = aventureiroService.criar(dto);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(AventureiroMapper.toResponse(aventureiro));
+    }
+}
