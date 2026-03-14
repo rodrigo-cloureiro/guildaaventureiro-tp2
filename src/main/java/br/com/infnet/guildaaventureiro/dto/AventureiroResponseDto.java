636358e324be53937c74310653613dec9e0e48d1
rodrigo.loureiro <rodrigo.loureiro@sondotecnica.com.br>
2026-03-14 15:44:20 -0300

    adicionando construtor no DTO de resposta da entidade Aventureiro

--- src/main/java/br/com/infnet/guildaaventureiro/dto/AventureiroResponseDto.java
+++ src/main/java/br/com/infnet/guildaaventureiro/dto/AventureiroResponseDto.java
@@ -1,5 +1,6 @@
 package br.com.infnet.guildaaventureiro.dto;
 
+import br.com.infnet.guildaaventureiro.domain.aventura.Aventureiro;
 import br.com.infnet.guildaaventureiro.domain.aventura.enums.AventureiroClasse;
 
 public record AventureiroResponseDto(
@@ -9,4 +10,13 @@ public record AventureiroResponseDto(
         int nivel,
         boolean ativo
 ) {
+    public AventureiroResponseDto(Aventureiro aventureiro) {
+        this(
+                aventureiro.getId(),
+                aventureiro.getNome(),
+                aventureiro.getClasse(),
+                aventureiro.getNivel(),
+                aventureiro.isAtivo()
+        );
+    }
 }
