397e8ac88954d8256e37c8f3b063407ddf49dda5
rodrigo.loureiro <rodrigo.loureiro@sondotecnica.com.br>
2026-03-14 18:34:04 -0300

    alterando tipo to 'total' de int para long

--- src/main/java/br/com/infnet/guildaaventureiro/dto/PagedResponse.java
+++ src/main/java/br/com/infnet/guildaaventureiro/dto/PagedResponse.java
@@ -5,7 +5,7 @@ import java.util.List;
 public record PagedResponse<T>(
         int page,
         int size,
-        int total,
+        long total,
         int totalPages,
         List<T> content
 ) {
