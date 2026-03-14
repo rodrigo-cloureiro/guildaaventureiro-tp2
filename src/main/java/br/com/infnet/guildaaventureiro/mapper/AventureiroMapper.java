664f951cc63155a8c81243a76cce789384d208ff
rodrigo.loureiro <rodrigo.loureiro@sondotecnica.com.br>
2026-03-14 16:29:05 -0300

    adiciona método toProfileResponse no AventureiroMapper

--- src/main/java/br/com/infnet/guildaaventureiro/mapper/AventureiroMapper.java
+++ src/main/java/br/com/infnet/guildaaventureiro/mapper/AventureiroMapper.java
@@ -1,7 +1,9 @@
 package br.com.infnet.guildaaventureiro.mapper;
 
 import br.com.infnet.guildaaventureiro.domain.aventura.Aventureiro;
+import br.com.infnet.guildaaventureiro.domain.aventura.Missao;
 import br.com.infnet.guildaaventureiro.dto.AventureiroCreateDto;
+import br.com.infnet.guildaaventureiro.dto.AventureiroProfileResponse;
 import br.com.infnet.guildaaventureiro.dto.AventureiroResponseDto;
 import org.springframework.stereotype.Component;
 
@@ -16,12 +18,21 @@ public class AventureiroMapper {
     }
 
     public static AventureiroResponseDto toResponse(Aventureiro aventureiro) {
-        return new AventureiroResponseDto(
-                aventureiro.getId(),
-                aventureiro.getNome(),
-                aventureiro.getClasse(),
-                aventureiro.getNivel(),
-                aventureiro.isAtivo()
+        return new AventureiroResponseDto(aventureiro);
+    }
+
+    public static AventureiroProfileResponse toProfileResponse(
+            Aventureiro aventureiro,
+            long totalParticipacoesEmMissao,
+            Missao missao
+    ) {
+        return new AventureiroProfileResponse(
+                toResponse(aventureiro),
+                aventureiro.getCompanheiro() != null ?
+                        CompanheiroMapper.toResponse(aventureiro.getCompanheiro()) :
+                        null,
+                totalParticipacoesEmMissao,
+                missao != null ? MissaoMapper.toResponse(missao) : null
         );
     }
 }
