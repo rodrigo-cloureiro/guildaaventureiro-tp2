20ef6f98d2b66b7ada766ff49559dd0b8021a812
rodrigo.loureiro <rodrigo.loureiro@sondotecnica.com.br>
2026-03-14 15:47:56 -0300

    implementa os metodos de consulta countByAventureiroId e findTopByAventureiroIdOrderByDataRegistroDesc em ParticipacaoMissaoRepository

--- src/main/java/br/com/infnet/guildaaventureiro/repository/aventura/ParticipacaoMissaoRepository.java
+++ src/main/java/br/com/infnet/guildaaventureiro/repository/aventura/ParticipacaoMissaoRepository.java
@@ -4,6 +4,11 @@ import br.com.infnet.guildaaventureiro.domain.aventura.ParticipacaoMissao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ParticipacaoMissaoRepository extends JpaRepository<ParticipacaoMissao, Long> {
+    long countByAventureiroId(Long id);
+
+    Optional<ParticipacaoMissao> findTopByAventureiroIdOrderByDataRegistroDesc(Long id);
 }
