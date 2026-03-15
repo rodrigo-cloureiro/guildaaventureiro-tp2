f54afc5b134684000dcc8094b7cd54647d95a0ff
rodrigo.loureiro <rodrigo.loureiro@sondotecnica.com.br>
2026-03-14 23:34:51 -0300

    adiciona a anotação @EqualsAndHashCode e define serialVersionUID para serializacao da classe

--- src/main/java/br/com/infnet/guildaaventureiro/domain/audit/UsuarioRoleId.java
+++ src/main/java/br/com/infnet/guildaaventureiro/domain/audit/UsuarioRoleId.java
@@ -2,15 +2,21 @@ package br.com.infnet.guildaaventureiro.domain.audit;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 @Embeddable
 @Getter
 @Setter
+@EqualsAndHashCode
 public class UsuarioRoleId implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
     @Column(name = "usuario_id", nullable = false)
     private Long usuarioId;
 
--- src/main/java/br/com/infnet/guildaaventureiro/domain/aventura/ParticipacaoMissaoId.java
+++ src/main/java/br/com/infnet/guildaaventureiro/domain/aventura/ParticipacaoMissaoId.java
@@ -2,15 +2,21 @@ package br.com.infnet.guildaaventureiro.domain.aventura;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 @Embeddable
 @Getter
 @Setter
+@EqualsAndHashCode
 public class ParticipacaoMissaoId implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
     @Column(name = "missao_id", nullable = false)
     private Long missaoId;
 
