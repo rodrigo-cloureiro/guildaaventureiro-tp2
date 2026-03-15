826b50e50e2923eb147e2b96bfb46847c9bc44e4
rodrigo.loureiro <rodrigo.loureiro@sondotecnica.com.br>
2026-03-15 03:48:16 -0300

    renomeia o método toResponse para toMinimalResponse e implementa o método toResponse retornando DTO MissaoResponse

--- src/main/java/br/com/infnet/guildaaventureiro/mapper/MissaoMapper.java
+++ src/main/java/br/com/infnet/guildaaventureiro/mapper/MissaoMapper.java
@@ -2,9 +2,14 @@ package br.com.infnet.guildaaventureiro.mapper;
 
 import br.com.infnet.guildaaventureiro.domain.aventura.Missao;
 import br.com.infnet.guildaaventureiro.dto.missao.MissaoMinimalResponse;
+import br.com.infnet.guildaaventureiro.dto.missao.MissaoResponse;
 
 public class MissaoMapper {
-    public static MissaoMinimalResponse toResponse(Missao missao) {
+    public static MissaoMinimalResponse toMinimalResponse(Missao missao) {
         return new MissaoMinimalResponse(missao);
+    }
+
+    public static MissaoResponse toResponse(Missao missao) {
+        return new MissaoResponse(missao);
     }
 }
