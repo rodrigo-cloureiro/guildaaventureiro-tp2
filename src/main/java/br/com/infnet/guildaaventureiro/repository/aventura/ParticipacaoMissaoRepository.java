7234dc41ab76ac2b817056923bba7b9701f1495e
rodrigo.loureiro <rodrigo.loureiro@sondotecnica.com.br>
2026-03-15 04:00:16 -0300

    adiciona o método de consulta findParticipantesByMissaoId que retorna o DTO AventureiroMissaoResponse com os detalhes da participacao do aventureiro na missao

--- src/main/java/br/com/infnet/guildaaventureiro/repository/aventura/ParticipacaoMissaoRepository.java
+++ src/main/java/br/com/infnet/guildaaventureiro/repository/aventura/ParticipacaoMissaoRepository.java
@@ -1,9 +1,13 @@
 package br.com.infnet.guildaaventureiro.repository.aventura;
 
 import br.com.infnet.guildaaventureiro.domain.aventura.ParticipacaoMissao;
+import br.com.infnet.guildaaventureiro.dto.AventureiroMissaoResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +15,19 @@ public interface ParticipacaoMissaoRepository extends JpaRepository<Participacao
     long countByAventureiroId(Long id);
 
     Optional<ParticipacaoMissao> findTopByAventureiroIdOrderByDataRegistroDesc(Long id);
+
+    @Query(value = """
+            SELECT new br.com.infnet.guildaaventureiro.dto.AventureiroMissaoResponse(
+                        a.nome,
+                        a.classe,
+                        a.nivel,
+                        pm.papel,
+                        pm.recompensaEmOuro,
+                        pm.mvp
+            )
+            FROM ParticipacaoMissao pm
+            JOIN pm.aventureiro a
+            WHERE pm.missao.id = :id
+            """)
+    List<AventureiroMissaoResponse> findParticipantesByMissaoId(@Param(value = "id") Long id);
 }
