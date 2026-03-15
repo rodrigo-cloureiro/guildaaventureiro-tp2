d1e83f4ef14b18de76f3d64e2d83afa9e200abe8
rodrigo.loureiro <rodrigo.loureiro@sondotecnica.com.br>
2026-03-15 04:02:15 -0300

    adiciona no servico o método missaoDetalhada que retorna a missao com detalhes

--- src/main/java/br/com/infnet/guildaaventureiro/service/MissaoService.java
+++ src/main/java/br/com/infnet/guildaaventureiro/service/MissaoService.java
@@ -1,19 +1,32 @@
 package br.com.infnet.guildaaventureiro.service;
 
+import br.com.infnet.guildaaventureiro.domain.aventura.Missao;
+import br.com.infnet.guildaaventureiro.dto.AventureiroMissaoResponse;
 import br.com.infnet.guildaaventureiro.dto.PagedResponse;
+import br.com.infnet.guildaaventureiro.dto.missao.MissaoDetailedResponse;
 import br.com.infnet.guildaaventureiro.dto.missao.MissaoFiltroRequest;
 import br.com.infnet.guildaaventureiro.dto.missao.MissaoResponse;
+import br.com.infnet.guildaaventureiro.mapper.MissaoMapper;
 import br.com.infnet.guildaaventureiro.repository.aventura.MissaoRepository;
+import br.com.infnet.guildaaventureiro.repository.aventura.ParticipacaoMissaoRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class MissaoService {
     private final MissaoRepository missaoRepository;
+    private final ParticipacaoMissaoRepository participacaoMissaoRepository;
 
+    // ==============
+    // Listar Missões
+    // ==============
     public PagedResponse<MissaoResponse> listar(MissaoFiltroRequest filtro, Pageable pageable) {
         Page<MissaoResponse> responsePage = missaoRepository.findByFilters(
                 filtro.status(),
@@ -27,5 +40,18 @@ public class MissaoService {
                 responsePage.getTotalPages(),
                 responsePage.getContent()
         );
+    }
+
+    // ====================
+    // Buscar Missão por ID
+    // ====================
+    @Transactional(readOnly = true)
+    public MissaoDetailedResponse missaoDetalhada(Long id) {
+        Missao missao = missaoRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Missão não encontrada"));
+
+        List<AventureiroMissaoResponse> participantes = participacaoMissaoRepository.findParticipantesByMissaoId(id);
+
+        return new MissaoDetailedResponse(MissaoMapper.toResponse(missao), participantes);
     }
 }
