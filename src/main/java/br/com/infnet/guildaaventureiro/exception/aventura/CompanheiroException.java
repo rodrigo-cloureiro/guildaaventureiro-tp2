eb1b9c815f2769cdf55095636d4c7e301e5947d0
rodrigo.loureiro <rodrigo.loureiro@sondotecnica.com.br>
2026-03-15 23:49:40 -0300

    adiciona a excecao CompanheiroException

---
+++ src/main/java/br/com/infnet/guildaaventureiro/exception/aventura/CompanheiroException.java
@@ -0,0 +1,7 @@
+package br.com.infnet.guildaaventureiro.exception.aventura;
+
+public class CompanheiroException extends RuntimeException {
+    public CompanheiroException(String message) {
+        super(message);
+    }
+}
