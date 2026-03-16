cf736d66c3db75fd96af37f01a88ce6b844ab511
rodrigo.loureiro <rodrigo.loureiro@sondotecnica.com.br>
2026-03-15 23:33:40 -0300

    movendo CompanheiroResponse para dto.companheiro

--- src/main/java/br/com/infnet/guildaaventureiro/dto/companheiro/CompanheiroResponse.java
+++ src/main/java/br/com/infnet/guildaaventureiro/dto/companheiro/CompanheiroResponse.java
@@ -1,4 +1,4 @@
-package br.com.infnet.guildaaventureiro.dto;
+package br.com.infnet.guildaaventureiro.dto.companheiro;
 
 import br.com.infnet.guildaaventureiro.domain.aventura.Companheiro;
 import br.com.infnet.guildaaventureiro.domain.aventura.enums.CompanheiroEspecie;
