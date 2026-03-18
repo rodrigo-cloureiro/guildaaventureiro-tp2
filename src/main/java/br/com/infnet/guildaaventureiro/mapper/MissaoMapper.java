f2d56d8d2468bdfd4ce43fc80d2eaa913b491af5
rodrigo.loureiro <rodrigo.loureiro@sondotecnica.com.br>
2026-03-18 17:15:28 -0300

    adiciona o mapeamento do DTO de criacao para a entidade Missao

--- src/main/java/br/com/infnet/guildaaventureiro/mapper/MissaoMapper.java
+++ src/main/java/br/com/infnet/guildaaventureiro/mapper/MissaoMapper.java
@@ -1,10 +1,20 @@
 package br.com.infnet.guildaaventureiro.mapper;
 
+import br.com.infnet.guildaaventureiro.domain.audit.Organizacao;
 import br.com.infnet.guildaaventureiro.domain.aventura.Missao;
+import br.com.infnet.guildaaventureiro.dto.MissaoCreate;
 import br.com.infnet.guildaaventureiro.dto.missao.MissaoMinimalResponse;
 import br.com.infnet.guildaaventureiro.dto.missao.MissaoResponse;
 
 public class MissaoMapper {
+    public static Missao toMissao(MissaoCreate dto, Organizacao organizacao) {
+        return new Missao(
+                organizacao,
+                dto.titulo(),
+                dto.nivelPerigo()
+        );
+    }
+
     public static MissaoMinimalResponse toMinimalResponse(Missao missao) {
         return new MissaoMinimalResponse(missao);
     }
