ad02d058ec3374ff61a37acc486e8128d68429e6
rodrigo.loureiro <rodrigo.loureiro@sondotecnica.com.br>
2026-03-18 16:53:32 -0300

    cria DTO para atualização de informacoes

---
+++ src/main/java/br/com/infnet/guildaaventureiro/dto/aventureiro/AventureiroUpdate.java
@@ -0,0 +1,14 @@
+package br.com.infnet.guildaaventureiro.dto.aventureiro;
+
+import br.com.infnet.guildaaventureiro.domain.aventura.enums.AventureiroClasse;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
+
+public record AventureiroUpdate(
+        @Size(max = 120, message = "O nome deve possuir no máximo 120 caracteres")
+        String nome,
+        AventureiroClasse classe,
+        @Min(value = 1, message = "O nível deve ser maior ou igual a 1")
+        Integer nivel
+) {
+}
