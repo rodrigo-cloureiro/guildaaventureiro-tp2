ab5b792af39d3d9e02f498e16b239e721d9bf6bb
rodrigo.loureiro <rodrigo.loureiro@sondotecnica.com.br>
2026-03-18 19:40:17 -0300

    cria DTO para atualização de informacoes da Missao

---
+++ src/main/java/br/com/infnet/guildaaventureiro/dto/missao/MissaoUpdate.java
@@ -0,0 +1,12 @@
+package br.com.infnet.guildaaventureiro.dto.missao;
+
+import br.com.infnet.guildaaventureiro.domain.aventura.enums.NivelPerigoMissao;
+import jakarta.validation.constraints.Size;
+
+public record MissaoUpdate(
+        @Size(max = 150, message = "O nome deve possuir no máximo 150 caracteres")
+        String titulo,
+
+        NivelPerigoMissao nivelPerigo
+) {
+}
