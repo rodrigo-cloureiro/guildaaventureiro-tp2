26bc0f0664ded27fbcc415bf20ebe510bde27cbd
rodrigo.loureiro <rodrigo.loureiro@sondotecnica.com.br>
2026-03-18 17:13:15 -0300

    adiciona DTO para criação de missões

---
+++ src/main/java/br/com/infnet/guildaaventureiro/dto/MissaoCreate.java
@@ -0,0 +1,20 @@
+package br.com.infnet.guildaaventureiro.dto;
+
+import br.com.infnet.guildaaventureiro.domain.aventura.enums.NivelPerigoMissao;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record MissaoCreate(
+        @Min(value = 1, message = "O ID não pode ser zero ou negativo")
+        Long organizacaoId,
+
+        @NotBlank(message = "O nome não pode ser vazio")
+        @Size(max = 150, message = "O nome deve possuir no máximo 150 caracteres")
+        String titulo,
+
+        @NotNull(message = "O nível de perigo da missão é obrigatório")
+        NivelPerigoMissao nivelPerigo
+) {
+}
