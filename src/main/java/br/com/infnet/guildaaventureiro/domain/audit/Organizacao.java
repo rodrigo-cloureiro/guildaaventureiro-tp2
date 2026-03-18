ac981369d1993f1d40031527996d3d2a2a27003f
rodrigo.loureiro <rodrigo.loureiro@sondotecnica.com.br>
2026-03-18 19:52:16 -0300

    adiciona relacionamento OneToMany de Organizacao para Missao

--- src/main/java/br/com/infnet/guildaaventureiro/domain/audit/Organizacao.java
+++ src/main/java/br/com/infnet/guildaaventureiro/domain/audit/Organizacao.java
@@ -1,6 +1,7 @@
 package br.com.infnet.guildaaventureiro.domain.audit;
 
 import br.com.infnet.guildaaventureiro.domain.aventura.Aventureiro;
+import br.com.infnet.guildaaventureiro.domain.aventura.Missao;
 import jakarta.persistence.*;
 import lombok.Getter;
 import org.hibernate.annotations.CreationTimestamp;
@@ -42,6 +43,9 @@ public class Organizacao {
 
     @OneToMany(mappedBy = "organizacao", fetch = FetchType.LAZY)
     private final Set<Usuario> usuarios = new HashSet<>();
+
+    @OneToMany(mappedBy = "organizacao", fetch = FetchType.LAZY)
+    private final Set<Missao> missoes = new HashSet<>();
 
     @OneToMany(mappedBy = "organizacao", fetch = FetchType.LAZY)
     private final Set<Role> roles = new HashSet<>();
