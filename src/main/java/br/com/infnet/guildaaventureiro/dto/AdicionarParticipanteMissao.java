b5d706dbf35ba09d9ba0b0a14237d0ca8e47d2b5
rodrigo.loureiro <rodrigo.loureiro@sondotecnica.com.br>
2026-03-19 00:23:22 -0300

    cria o DTO para adição de participantes em missões

---
+++ src/main/java/br/com/infnet/guildaaventureiro/dto/AdicionarParticipanteMissao.java
@@ -0,0 +1,14 @@
+package br.com.infnet.guildaaventureiro.dto;
+
+import br.com.infnet.guildaaventureiro.domain.aventura.enums.PapelMissao;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+public record AdicionarParticipanteMissao(
+        @Min(value = 1, message = "O ID do participante não pode ser zero ou negativo")
+        Long participanteId,
+
+        @NotNull(message = "O papel na missão é obrigatório")
+        PapelMissao papelMissao
+) {
+}
