a22e0211ce945d372541083d9030f203524161fd
rodrigo.loureiro <rodrigo.loureiro@sondotecnica.com.br>
2026-03-21 14:46:25 -0300

    cria método de consulta que retorna o raking de participacao

--- src/main/java/br/com/infnet/guildaaventureiro/repository/aventura/ParticipacaoMissaoRepository.java
+++ src/main/java/br/com/infnet/guildaaventureiro/repository/aventura/ParticipacaoMissaoRepository.java
@@ -2,12 +2,15 @@ package br.com.infnet.guildaaventureiro.repository.aventura;
 
 import br.com.infnet.guildaaventureiro.domain.aventura.ParticipacaoMissao;
 import br.com.infnet.guildaaventureiro.domain.aventura.ParticipacaoMissaoId;
+import br.com.infnet.guildaaventureiro.domain.aventura.enums.StatusMissao;
+import br.com.infnet.guildaaventureiro.dto.relatorio.RankingParticipacao;
 import br.com.infnet.guildaaventureiro.dto.aventureiro.AventureiroMissaoResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -45,4 +48,27 @@ public interface ParticipacaoMissaoRepository extends JpaRepository<Participacao
             WHERE pm.missao.id = :id
             """)
     List<AventureiroMissaoResponse> findParticipantesByMissaoId(@Param(value = "id") Long id);
+
+    @Query(value = """
+            SELECT new br.com.infnet.guildaaventureiro.dto.relatorio.RankingParticipacao(
+                        a.nome,
+                        COUNT(pm.missao.id),
+                        SUM(pm.recompensaEmOuro),
+                        SUM(CASE WHEN pm.mvp = true THEN 1 ELSE 0 END)
+                    )
+            FROM ParticipacaoMissao pm
+            JOIN pm.aventureiro a
+            WHERE (:status IS NULL OR pm.missao.status = :status)
+            AND pm.missao.dataInicio >= :inicio
+            AND (pm.missao.dataTermino <= :termino OR pm.missao.dataTermino IS NULL)
+            GROUP BY a.id, a.nome
+            ORDER BY SUM(pm.recompensaEmOuro) DESC NULLS LAST,
+                     SUM(CASE WHEN pm.mvp = true THEN 1 ELSE 0 END) DESC,
+                     COUNT(pm.missao.id) DESC
+            """)
+    List<RankingParticipacao> rankingParticipacao(
+            @Param(value = "status") StatusMissao status,
+            @Param(value = "inicio") LocalDateTime inicio,
+            @Param(value = "termino") LocalDateTime termino
+    );
 }
