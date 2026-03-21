10da835b1d8832c4379abb44b202c7a8efeec714
rodrigo.loureiro <rodrigo.loureiro@sondotecnica.com.br>
2026-03-21 14:57:31 -0300

    cria o endpoint GET /ranking-participacao

---
+++ src/main/java/br/com/infnet/guildaaventureiro/controller/RelatorioController.java
@@ -0,0 +1,29 @@
+package br.com.infnet.guildaaventureiro.controller;
+
+import br.com.infnet.guildaaventureiro.dto.relatorio.RankingParticipacao;
+import br.com.infnet.guildaaventureiro.dto.relatorio.RankingPartipacaoFiltroRequest;
+import br.com.infnet.guildaaventureiro.service.RelatorioService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/relatorios")
+public class RelatorioController {
+    private final RelatorioService relatorioService;
+
+    // =======================
+    // Ranking de Participação
+    // =======================
+    @GetMapping(value = "/ranking-participacao")
+    public ResponseEntity<List<RankingParticipacao>> rankingParticipacao(@Valid RankingPartipacaoFiltroRequest filtro) {
+        return ResponseEntity.ok()
+                .body(relatorioService.rankingParticipacao(filtro));
+    }
+}
