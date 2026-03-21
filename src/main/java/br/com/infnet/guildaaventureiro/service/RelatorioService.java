c48b79bd44bec2913c54fcdae3ef73dc078530a9
rodrigo.loureiro <rodrigo.loureiro@sondotecnica.com.br>
2026-03-21 14:48:09 -0300

    cria o servico rankingParticipacao

---
+++ src/main/java/br/com/infnet/guildaaventureiro/service/RelatorioService.java
@@ -0,0 +1,38 @@
+package br.com.infnet.guildaaventureiro.service;
+
+import br.com.infnet.guildaaventureiro.dto.relatorio.RankingParticipacao;
+import br.com.infnet.guildaaventureiro.dto.relatorio.RankingPartipacaoFiltroRequest;
+import br.com.infnet.guildaaventureiro.repository.aventura.ParticipacaoMissaoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class RelatorioService {
+    public static final int INTERVALO_DIAS_PADRAO = 30;
+
+    private final ParticipacaoMissaoRepository participacaoMissaoRepository;
+
+    // =======================
+    // Ranking de Participação
+    // =======================
+    public List<RankingParticipacao> rankingParticipacao(RankingPartipacaoFiltroRequest filtro) {
+        LocalDateTime now = LocalDateTime.now();
+
+        LocalDateTime inicio = filtro.inicio() == null ? now.minusDays(INTERVALO_DIAS_PADRAO) : filtro.inicio();
+        LocalDateTime termino = filtro.termino() == null ? now : filtro.termino();
+
+        if (termino.isBefore(inicio)) {
+            throw new IllegalStateException("A data de término não pode ser anterior a data de início");
+        }
+
+        return participacaoMissaoRepository.rankingParticipacao(
+                filtro.status(),
+                inicio,
+                termino
+        );
+    }
+}
