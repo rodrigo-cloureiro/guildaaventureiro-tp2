17de8293987df4ea6883552e6a09e02ab2b1f021
rodrigo.loureiro <rodrigo.loureiro@sondotecnica.com.br>
2026-03-22 01:42:59 -0300

    adiciona handler global para as excecoes

---
+++ src/main/java/br/com/infnet/guildaaventureiro/advice/ApiExceptionHandler.java
@@ -0,0 +1,50 @@
+package br.com.infnet.guildaaventureiro.advice;
+
+import br.com.infnet.guildaaventureiro.dto.ErrorResponse;
+import br.com.infnet.guildaaventureiro.exception.aventura.*;
+import jakarta.persistence.EntityNotFoundException;
+import org.jspecify.annotations.Nullable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+
+@RestControllerAdvice
+public class ApiExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException ex) {
+        List<@Nullable String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(FieldError::getDefaultMessage)
+                .toList();
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(
+                        "Falha na validação da requisição",
+                        errors
+                ));
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleEntityNotFoundException(EntityNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new ErrorResponse(
+                        "Recurso não encontrado",
+                        List.of(ex.getMessage())
+                ));
+    }
+
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<ErrorResponse> handleBusinessExceptions(BusinessException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(
+                        "Erro de regra de negócio",
+                        List.of(ex.getMessage())
+                ));
+    }
+}
