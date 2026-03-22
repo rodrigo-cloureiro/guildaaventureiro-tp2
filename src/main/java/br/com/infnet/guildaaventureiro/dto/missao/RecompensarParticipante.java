d803c4b57e983a8f35d7b39a7456c99dca03e74a
rodrigo.loureiro <rodrigo.loureiro@sondotecnica.com.br>
2026-03-22 02:20:34 -0300

    refatora a validacao do valor da recompensa

--- src/main/java/br/com/infnet/guildaaventureiro/dto/missao/RecompensarParticipante.java
+++ src/main/java/br/com/infnet/guildaaventureiro/dto/missao/RecompensarParticipante.java
@@ -3,7 +3,7 @@ package br.com.infnet.guildaaventureiro.dto.missao;
 import jakarta.validation.constraints.Min;
 
 public record RecompensarParticipante(
-        @Min(value = '1', message = "A recompensa deve ser um valor positivo")
+        @Min(value = 1, message = "A recompensa deve ser um valor positivo")
         int recompensa
 ) {
 }
