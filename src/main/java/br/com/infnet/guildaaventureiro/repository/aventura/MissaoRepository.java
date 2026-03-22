eefd83d820a3611893af2cb24d9508859899f97e
rodrigo.loureiro <rodrigo.loureiro@sondotecnica.com.br>
2026-03-22 00:45:13 -0300

    adiciona filtro por periodo na query e renomeia o método para findMissoesByFilters

--- src/main/java/br/com/infnet/guildaaventureiro/repository/aventura/MissaoRepository.java
+++ src/main/java/br/com/infnet/guildaaventureiro/repository/aventura/MissaoRepository.java
@@ -11,17 +11,28 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+
 @Repository
 public interface MissaoRepository extends JpaRepository<Missao, Long> {
     @Query(value = """
             SELECT new br.com.infnet.guildaaventureiro.dto.missao.MissaoResponse(m)
             FROM Missao m
-            WHERE (:status IS NULL OR m.status = :status) AND
-            (:nivelPerigo IS NULL OR m.nivelPerigo = :nivelPerigo)
+            WHERE (:status IS NULL OR m.status = :status)
+            AND (:nivelPerigo IS NULL OR m.nivelPerigo = :nivelPerigo)
+            AND (
+                    (:tipoData IS NULL) OR
+                    (:tipoData = 'CRIACAO' AND m.dataCriacao BETWEEN :de AND :ate) OR
+                    (:tipoData = 'INICIO' AND m.dataInicio BETWEEN :de AND :ate) OR
+                    (:tipoData = 'TERMINO' AND m.dataTermino BETWEEN :de AND :ate)
+            )
             """)
-    Page<MissaoResponse> findByFilters(
+    Page<MissaoResponse> findMissoesByFilters(
             @Param(value = "status") StatusMissao status,
             @Param(value = "nivelPerigo") NivelPerigoMissao nivelPerigo,
+            @Param("tipoData") String tipoData,
+            @Param("de") LocalDateTime de,
+            @Param("ate") LocalDateTime ate,
             Pageable pageable
     );
 }
