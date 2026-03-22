c77de3cad4ce6b760e71e7dd02cf147cefe9aed5
rodrigo.loureiro <rodrigo.loureiro@sondotecnica.com.br>
2026-03-22 01:40:03 -0300

    cria excecao de regra de negocio

---
+++ src/main/java/br/com/infnet/guildaaventureiro/exception/aventura/BusinessException.java
@@ -0,0 +1,7 @@
+package br.com.infnet.guildaaventureiro.exception.aventura;
+
+public class BusinessException extends RuntimeException {
+    public BusinessException(String message) {
+        super(message);
+    }
+}
